c2ea052238e86f2d0135c2cb1fcd4ee752b13ccc
srinavadeepkondasani <kondasanisrinavadeep@gmail.com>
2025-10-01 18:59:15 +0530

    Update dayone.py

--- dayone.py
+++ dayone.py
@@ -1,1 +1,14 @@
-print("Today is Day One")
+# for i in range(51,101):
+    # print(i)
+# j=1
+# while(j<6):
+#     print(j)
+#     j=j+1;
+# n=int(input("Enter a Number:"))
+# if n%2==0:
+#     print("It is a even Number")
+# else:
+#         print("It is a odd Number")
+# name1=(input("Enter your name:"))
+# name2=(input("Enter your name:"))
+
